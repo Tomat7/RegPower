d9180927174e0fae3d759680a50d60f455dcaee0
Tomat7 <32893720+Tomat7@users.noreply.github.com>
2017-12-27 12:45:09 +0300

    Update RegPower.h

--- RegPower.h
+++ RegPower.h
@@ -10,15 +10,15 @@
 
 #include "Arduino.h"
 
-#define LIBVERSION "RegPower_v20171123 R="
-#define ZEROOFFSET 10         // Сдвиг срабатывания детектора нуля относительно реального нуля в тиках таймера
-#define C_TIMER 625-ZEROOFFSET          // Максимальное количество срабатываний таймера за полупериод синусоиды
+#define LIBVERSION "RegPower_v20171225 R="
+#define ZEROOFFSET 40         // Сдвиг срабатывания детектора нуля относительно реального нуля в тиках таймера
+#define C_TIMER 2500-ZEROOFFSET          // Максимальное количество срабатываний таймера за полупериод синусоиды
 #define ACS_RATIO 0.048828125 // Коэффициент датчика ACS712 |5А - 0.024414063 | 20А - 0.048828125 | 30A - 0.073242188 |
 #define ZCROSS 3
 #define TRIAC PORTD5	// пока не проверялось! возможно дальше порт прямо указан в программе!
 
-#define LAG_FACTOR 30	// Коэффициент интеграции в расчете угла (defaul 300)
-#define AVG_FACTOR 5	// Фактор усреднения измеренного тока (defaul 5)
+#define LAG_FACTOR 50	// Коэффициент интеграции в расчете угла (defaul 300)
+#define AVG_FACTOR 2	// Фактор усреднения измеренного тока (defaul 5)
 
 class RegPower
 {
@@ -37,12 +37,16 @@ public:
 	static void ZeroCross_int() __attribute__((always_inline));
 	static void GetI_int() __attribute__((always_inline));
 	static void SetTriac_int() __attribute__((always_inline));
+	// === test
+	uint16_t LagFactor;
+	uint16_t ADCperiod;
 
 protected:
-	volatile static bool _zero;
+	volatile static bool _getI;
 	volatile static int _cntr;
 	volatile static unsigned long _Isumm;
 	volatile static float _angle = C_TIMER;
+	unsigned long _ADCmillis;
 
 };
 extern RegPower TEH;
